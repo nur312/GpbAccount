82bb27d26fbaeafb1f320558cc963571b5ed7be2
nur312 <nurzhigit.en@gmail.com>
2022-02-07 21:30:48 +0300

    dependencies were added

---
+++ src/test/java/gpb/account/GpbAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package gpb.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpbAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
