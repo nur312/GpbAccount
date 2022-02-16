a7daccc179c6630e05c634129e0c5301e0a2c65c
VasyaMuking <cosmorw@yandex.ru>
2022-02-16 18:17:34 +0300

    added createAccount realization. Reworked Account.

--- src/main/java/gpb/account/repo/AccountRepo.java
+++ src/main/java/gpb/account/repo/AccountRepo.java
@@ -1,10 +1,12 @@
 package gpb.account.repo;
 
+import gpb.account.entity.BankAccountEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import gpb.account.entity.AccountEntity;
 
 @Repository
 public interface AccountRepo extends JpaRepository<AccountEntity, Integer> {
-    AccountEntity findByClientId(Long clientId);
+    AccountEntity findByClientId(Integer clientId);
+
 }
