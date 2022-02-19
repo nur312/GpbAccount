cf8c97b03a2e5cc57a85fd37e2cffa3d0ecb1186
Boris <www-bory72@yandex.ru>
2022-02-19 23:37:00 +0300

    exceptions description was added

--- src/main/java/gpb/account/exception/AccountNotFoundException.java
+++ src/main/java/gpb/account/exception/AccountNotFoundException.java
@@ -1,4 +1,15 @@
 package gpb.account.exception;
 
 public class AccountNotFoundException extends RuntimeException {
+
+    private Integer accountNo;
+    public Integer getAccountNo(Integer accountNo){
+        return accountNo;
+    }
+
+    public AccountNotFoundException(String message, Integer accountNum){
+        super(message);
+        accountNo = accountNum;
+    }
+
 }
--- src/main/java/gpb/account/exception/NotSufficientFundsException.java
+++ src/main/java/gpb/account/exception/NotSufficientFundsException.java
@@ -1,4 +1,17 @@
 package gpb.account.exception;
 
 public class NotSufficientFundsException extends RuntimeException {
+
+    private Integer accountNo;
+    public Integer getAccountNo(Integer accountNo){
+        return accountNo;
+    }
+
+    public NotSufficientFundsException(String message, Integer accountNum){
+        super(message);
+        accountNo = accountNum;
+    }
+
+//    ToDo обсудить, надо ли вставлять данные о том, насколько не хватает денег(newActualBalance)
+
 }
